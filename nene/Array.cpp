e9b2643224a59c0e5aecfb90bf8a430dc1f02284
nnmei <kusumnipa.r@ku.th>
2025-09-24 19:07:34 +0700

    Array.cpp

--- nene/Array.cpp
+++ nene/Array.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-	char sentence[50];
-	char word[15];
+	char sentence[100];
+	char word[100];
 
 	printf("Input sentence : "); gets(sentence);
 	printf("Input word to compare : "); gets(word);
 
-	char cp[1][15];
+	char cp[50][50];
 	char cp2[50][50];
 	int lengths, count = 0, lengthw, location  = 0;
 	lengths = strlen(sentence);
@@ -19,12 +19,13 @@ int main() {
 	}
 	for(i=0; i < lengths; i++){
 		for(j=0; j < lengthw; j++){
-			cp2[location][j] = sentence[i+j];
+			cp2[i][j] = sentence[i+j];
 		}
-		location++;
+		cp2[i][lengthw] = '\0';
+		printf("%s\n",cp2[i]);
 	}
 
-	for(i=0; i<location; i++){
+	for(i=0; i<lengths; i++){
 		if(strcmp(cp[0],cp2[i]) == 0) {
 			count++;
 		}
